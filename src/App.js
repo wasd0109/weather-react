b0715180b531b8008906bd593f77903304d74b94
Ken Cheung <wasd0109.dev@gmail.com>
2020-06-23 16:20:25 +0800

    Create weather forecast card component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ForecastBlock from "./components/ForecastBlock";
+import "./App.css";
+import "./output.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      latitude: "",
+      longitude: "",
+    };
+  }
+
+  getGeolocation() {
+    const { latitude, longitude } = this.state;
+    if (navigator.geolocation && !latitude && !longitude) {
+      navigator.geolocation.getCurrentPosition((position) =>
+        this.setGeolocation(position)
+      );
+    }
+  }
+
+  setGeolocation(position) {
+    this.setState(
+      {
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude,
+      },
+      this.getWeather
+    );
+  }
+
+  getWeather() {
+    fetch(
+      `https://api.openweathermap.org/data/2.5/onecall?lat=${this.state.latitude}&lon=${this.state.longitude}&exclude=minutely,hourly,daily&appid=648f9c780527e242ca6a26f3b1b28467`
+    )
+      .then((response) => response.json())
+      .then(console.log);
+  }
+
+  componentDidMount() {
+    this.getGeolocation();
+  }
+
+  render() {
+    return (
+      <ForecastBlock
+        dt="1588935779"
+        timezone_offset="-18000"
+        temp="16.75"
+        feels_like="16.07"
+        humidity="93"
+        weatherDescription="moderate rain"
+        weatherIcon="10n"
+      />
+    );
+  }
 }
 
 export default App;
---
+++ src/components/ForecastBlock.js
@@ -0,0 +1,51 @@
+import React from "react";
+import moment from "moment";
+
+//dt,timezone_offset, temp,feels_like,humidity,weather.main/.description/.icon
+
+function ForecastBlock({
+  dt,
+  timezone_offset,
+  temp,
+  feels_like,
+  humidity,
+  weatherDescription,
+  weatherIcon,
+}) {
+  const monthOffset = 1;
+  let month = ("0" + (moment.unix(dt).month() + monthOffset)).slice(-2);
+  let date = ("0" + moment.unix(dt).date()).slice(-2);
+  return (
+    <div className="flex">
+      <div className="max-w-lg rounded overflow-hidden shadow-lg bg-blue-100 p-4">
+        <div className="flex">
+          <div>
+            <img
+              className="w-32"
+              src={`http://openweathermap.org/img/wn/${weatherIcon}@2x.png`}
+              alt="Current Weather"
+            />
+          </div>
+
+          <div className="justify-end m-0">
+            <p className="text-3xl">{`${temp}°C`}</p>
+            <p className="text-sm m-0">Feels Like:</p>
+            <p className="text-2xl">{`${feels_like}°C`}</p>
+            <p className=" text-3xl m-0">{`${month}/${date}`}</p>
+          </div>
+        </div>
+
+        <p className="ml-6">
+          Weather:{" "}
+          <span className="text-xl capitalize">{weatherDescription}</span>
+        </p>
+
+        <p className="ml-6">
+          Humidity: <span className="text-xl">{`${humidity}%`}</span>
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default ForecastBlock;
